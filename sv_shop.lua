165133c0ecea23f8537192372a2dbc4006a50e96
juddyyy <67974904+juddyyy@users.noreply.github.com>
2022-01-19 23:42:53 +0000

    Add files via upload

--- sv_shop.lua
+++ sv_shop.lua
@@ -8,9 +8,18 @@ AddEventHandler("vrpshop:buy", function(item, price)
     local source = source
     local user_id = vRP.getUserId({source})
 
-    if vRP.tryFullPayment({user_id, price}) then
-        vRP.giveInventoryItem({user_id, item, 1, true})
-    else
-        vRPclient.notify(source,{"~r~You do not have enough money to buy this!"})
+    for i, v in pairs(cfg.items) do
+        if item == v.itemid then
+            if price == v.price then
+                if vRP.tryFullPayment({user_id, price}) then
+                    vRP.giveInventoryItem({user_id, item, 1, true})
+                else
+                    vRPclient.notify(source,{"~r~You do not have enough money to buy this!"})
+                end
+            else
+                print("perm id: "..user_id.." tried to trigger vrpshop:buy event")
+                vRP.banConsole({user_id, "perm", "Triggering Events."})
+            end
+        end
     end
 end)
